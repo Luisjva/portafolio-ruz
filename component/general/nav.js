ae3a2b0ede2aacb1b7deb166dd03d49b9f8ac342
luisjva <luisjva16@gmail.com>
2022-02-15 15:03:53 -0400

    Inicios en el nav

---
+++ component/general/nav.js
@@ -0,0 +1,135 @@
+import Image from "next/image";
+import { useState } from "react";
+
+import { colors, contact } from "../../utils";
+
+export default function Nav() {
+  const [contactVisible, setContactVisible] = useState(false);
+
+  const clickContact = () => {
+    contactVisible ? setContactVisible(false) : setContactVisible(true);
+  };
+
+  return (
+    <nav>
+      <div>
+        <div>Logo</div>
+        <div className="nav__right">
+          <p>foto de joras</p>
+          <div className="nav__right__contact">
+            <button
+              className={
+                contactVisible
+                  ? "nav__right__contact__btn nav__right__contact__btn--open"
+                  : "nav__right__contact__btn"
+              }
+              onClick={() => clickContact()}
+            >
+              <Image src="/contact.png" alt="Send" width="24" height="24" />{" "}
+            </button>
+            {contact.map((cont, index) => {
+              return (
+                <a
+                  key={index}
+                  href="#"
+                  style={{ transition: 0.3 * (index + 1) + "s" }}
+                  className={
+                    contactVisible
+                      ? `nav__right__contact__link nav__right__contact__link__${
+                          index + 1
+                        }`
+                      : "nav__right__contact__link"
+                  }
+                >
+                  <Image src="/contact.png" alt="Send" width="20" height="20" />
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        nav {
+          align-items: center;
+          display: flex;
+          justify-content: center;
+          height: 3rem;
+        }
+
+        nav > div {
+          background: ${colors.primary};
+          color: #fff;
+          display: grid;
+          grid-template-columns: 1fr 2fr;
+          padding: 0 1rem;
+          width: 100%;
+        }
+
+        .nav__right {
+          align-items: center;
+          display: flex;
+          justify-content: flex-end;
+        }
+
+        .nav__right__contact {
+          position: relative;
+        }
+
+        .nav__right__contact__btn {
+          align-items: center;
+          background-color: ${colors.yellow};
+          border: none;
+          border-radius: 100px;
+          display: flex;
+          height: 40px;
+          justify-content: center;
+          margin-left: 1rem;
+          transform: rotate(-25deg);
+          transition: 0.3s;
+          width: 40px;
+        }
+
+        .nav__right__contact__btn:hover {
+          transform: scale(1.05) rotate(-30deg);
+        }
+
+        .nav__right__contact__btn--open {
+          transform: scale(1.15) rotate(-40deg);
+        }
+
+        .nav__right__contact__btn--open:hover {
+          transform: scale(1.05) rotate(-30deg);
+        }
+
+        .nav__right__contact__link {
+          align-items: center;
+          background-color: ${colors.yellow};
+          border-radius: 100px;
+          display: flex;
+          height: 35px;
+          justify-content: center;
+          position: absolute;
+          right: 5px;
+          top: -3.5rem;
+          width: 35px;
+        }
+
+        .nav__right__contact__link:hover {
+          transform: scale(1.1);
+        }
+
+        .nav__right__contact__link__1 {
+          top: 3.5rem;
+        }
+
+        .nav__right__contact__link__2 {
+          top: 6.5rem;
+        }
+
+        .nav__right__contact__link__3 {
+          top: 9.5rem;
+        }
+      `}</style>
+    </nav>
+  );
+}
---
+++ utils.js
@@ -0,0 +1,7 @@
+export const colors = {
+  primary: "#d40863",
+  yellow: "#e0ad3d",
+  negro: "#000000",
+};
+
+export const contact = [{}, {}, {}];
